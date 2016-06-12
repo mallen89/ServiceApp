cae0fbb4fd968411ad07fa73f4437546ec5e0c70
Matthew Allen <mallen0819@gmail.com>
2016-06-12 17:09:10 -0500

    Created Project
    Added all of the initial project files.

---
+++ ServiceApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceApp.Startup))]
+namespace ServiceApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
