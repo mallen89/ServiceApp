688ab2946e8df314cb268cf4cd794a23c71df6f8
Matthew Allen <mallen0819@gmail.com>
2016-06-12 19:24:23 -0500

    Added Clients Page
    Added clients page. Now able to add clients to database.

---
+++ ServiceApp/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace ServiceApp.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public long HomePhone { get; set; }
+        public long CellPhone { get; set; }
+        public long AlternatePhone { get; set; }
+    }
+
+    public class ServiceDBContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+    }
+}
